4e7ed38eef1e422b9c5064dd4e9d5ca1ac973812
geektechniquestudios <geektechniquestudios@gmail.com>
2022-11-25 18:38:36 -0500

    changed name of title in footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -35,7 +35,7 @@ export const Footer: React.FC<Props> = ({}) => {
           icon={<RiCodeSSlashFill />}
         />
         <SocialLink
-          title="Support"
+          title="Contact"
           icon={<BiSupport />}
           onClick={() => {
             MySwal.fire({
