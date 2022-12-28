be25cfc976f40dc106661436d9d4f2552d690b6e
geektechniquestudios <geektechniquestudios@gmail.com>
2022-12-27 21:47:29 -0500

    changed border color on header to blend in more

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -4,7 +4,7 @@ import { HeaderRight } from "./HeaderRight"
 
 export const Header: React.FC = () => {
   return (
-    <div className="sticky top-0 z-50 flex h-11 w-full shrink-0 items-center justify-center border-b border-stone-700 bg-sky-500 bg-opacity-10 text-xl text-stone-200 backdrop-blur-sm">
+    <div className="sticky top-0 z-50 flex h-11 w-full shrink-0 items-center justify-center border-b border-blue-600 border-opacity-10 bg-sky-500 bg-opacity-10 text-xl text-stone-200 backdrop-blur-sm">
       <div className="no-wrap flex w-full justify-between">
         <HeaderLeft />
         <HeaderMiddle />
